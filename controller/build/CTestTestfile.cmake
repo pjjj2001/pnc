fdbe337c54ba0eb18105f9b07fb7ced088e248cc
pjjj2001 <954838081@qq.com>
2024-05-26 19:55:51 +0800

    message

---
+++ controller/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/pj/pnc/controller/src
+# Build directory: /home/pj/pnc/controller/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ controller/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/pj/pnc/controller/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/pj/pnc/controller/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/pj/pnc/controller/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH='/home/pj/pnc/controller/src:/home/pj/catkin_ws/src/VINS-Fusion/camera_models:/home/pj/catkin_ws/src/gazebo_ros_pkgs/gazebo_dev:/home/pj/catkin_ws/src/gazebo_ros_pkgs/gazebo_msgs:/home/pj/catkin_ws/src/gazebo_ros_pkgs/gazebo_ros:/home/pj/catkin_ws/src/gazebo_ros_pkgs/gazebo_plugins:/home/pj/catkin_ws/src/gazebo_ros_pkgs/gazebo_ros_control:/home/pj/catkin_ws/src/gazebo_ros_pkgs/gazebo_ros_pkgs:/home/pj/catkin_ws/src/VINS-Fusion/global_fusion:/home/pj/catkin_ws/src/VINS-Fusion/loop_fusion:/home/pj/catkin_ws/src/ego_planner/plan_env:/home/pj/catkin_ws/src/ego_planner/path_searching:/home/pj/catkin_ws/src/ego_planner/traj_utils:/home/pj/catkin_ws/src/ego_planner/bspline_opt:/home/pj/catkin_ws/src/ego_planner/plan_manage:/home/pj/catkin_ws/src/VINS-Fusion/vins_estimator:/opt/ros/noetic/share'
